284bc5bd0c50387c2ab50df6c9dd4cca4958a90b
srcascudo <srcascudo.in@gmail.com>
2025-06-16 14:46:48 -0300

    'Teste'

---
+++ autoversion.py
@@ -0,0 +1,10 @@
+import subprocess
+
+p = subprocess.Popen('git branch', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+subprocess.Popen('git add .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+subprocess.Popen("git commit -m 'Teste'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+subprocess.Popen('git push', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+for line in p.stdout.readlines():
+    print(line, end=' \n')
+retval = p.wait()
