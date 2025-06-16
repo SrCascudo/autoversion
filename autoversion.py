3d781da371a27037869144ed913001acc16afcd4
srcascudo <srcascudo.in@gmail.com>
2025-06-16 16:34:58 -0300

    Adicionar data ao commit

--- autoversion.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-subprocess.Popen('cd c:/Users/johnhca/Documents/Repositorio/Pessoal/autoversion/ & git add .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-subprocess.Popen('cd c:/Users/johnhca/Documents/Repositorio/Pessoal/autoversion/ & git commit -m "Backup"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-subprocess.Popen('cd c:/Users/johnhca/Documents/Repositorio/Pessoal/autoversion/ & git push', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-# for line in p.stdout.readlines():
-#     print(line, end=' \n')
-# retval = p.wait()
---
+++ autoversion_contabilx.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import subprocess
+
+p = subprocess.Popen('y: & cd contabilx & git add .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    print(line, end=' \n')
+retval = p.wait()
+
+date = datetime.now().strftime("%d/%m/%Y")
+p = subprocess.Popen(f'y: & cd contabilx & git commit -m "Backup Automático {date}"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    print(line, end=' \n')
+retval = p.wait()
+
+
+p = subprocess.Popen('y: & cd contabilx & git push origin master:developer', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    print(line, end=' \n')
+retval = p.wait()
