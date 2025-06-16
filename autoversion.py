94cd923629d40639ed29f6329c890020247c1fb8
srcascudo <srcascudo.in@gmail.com>
2025-06-16 14:53:31 -0300

    srcascudo:backup automático

--- autoversion.py
+++ autoversion.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 import subprocess
 
 subprocess.Popen('git add .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-subprocess.Popen("git commit -m 'Teste'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+subprocess.Popen('git commit -m "srcascudo:backup automático"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 subprocess.Popen('git push', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 # for line in p.stdout.readlines():
